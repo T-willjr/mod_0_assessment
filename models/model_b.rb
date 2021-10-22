5177a6a0bc9d02ff7da997b1efe7af9cc87d2cb4
Timothy Williams <williamsjr.timothy1@gmail.com>
2021-10-22 03:06:45 -0400

    Add vehicle hash

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+car = {
+  make: "Honda",
+  model: "Accord",
+  seats: 5,
+  windows: 4,
+  has_flames: true,
+  is_lifted: true,
+  people_in_car: ["Jessica","James","Danica"],
+  pets_in_Car: ["Sir Charles", "Pepe", "Rose"]
+}
