04de5ad75596c400f46f64f39636bc2f7f47ff77
Timothy Williams <williamsjr.timothy1@gmail.com>
2021-10-22 03:13:17 -0400

    Add quotes to hash keys

--- models/model_a.rb
+++ models/model_a.rb
@@ -1,10 +1,10 @@
 AIRPORT = {
-  airline_one => "Delta",
-  airline_two => "Spirit",
-  num_of_planes => 5,
-  num_of_passengers => 200,
-  planes_available => true,
-  delayed_flights => false,
-  restaurants => ["Chili's", "Waffle House", "5 guys"],
-  security_personnel => ["Bob", "Angela", "Derek"]
+  "airline_one" => "Delta",
+  "airline_two" => "Spirit",
+  "num_of_planes" => 5,
+  "num_of_passengers" => 200,
+  "planes_available" => true,
+  "delayed_flights" => false,
+  "restaurants" => ["Chili's", "Waffle House", "5 guys"],
+  "security_personnel" => ["Bob", "Angela", "Derek"]
 }
