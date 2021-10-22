0dcb222f5491acf5dd573d66930e5fd769daa27a
Timothy Williams <williamsjr.timothy1@gmail.com>
2021-10-22 02:51:44 -0400

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,13 @@
+famous_person = "Johnny Depp"
+is_active = true
+
+famous_person = "Eric Andre"
+num_3 = 3
+num_9 = 9
+
+sum = num_9 + num_3
+
+animals = ["Elephant","Gorilla","Duck"]
+animals.shift
+
+puts famous_person.length 
