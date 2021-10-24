f46b51bc7e1dfd5bcf36b22e03abdb735c14d8bb
Timothy Williams <williamsjr.timothy1@gmail.com>
2021-10-24 19:52:32 -0400

    Correct variable names

--- models/model_b.rb
+++ models/model_b.rb
@@ -6,5 +6,5 @@ car = {
   has_flames: true,
   is_lifted: true,
   people_in_car: ["Jessica","James","Danica"],
-  pets_in_Car: ["Sir Charles", "Pepe", "Rose"]
+  pets_in_car: ["Sir Charles", "Pepe", "Rose"]
 }
